9820ea51bc3cedf9f62b5a10b6af6592f70c5eb3
AnBaToBun22 <phanquanghieusuper@gmail.com>
2026-04-12 07:37:09 +0700

    push web

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='Zecter@1234',
+        password='123456',
         database='lost_found_db'
     )
  
